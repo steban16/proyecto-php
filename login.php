b1115a767bc7fde608d5f9f5671cdc7ca531b2ae
steban16 <javierstebancastellanosmora@gmail.com>
2026-05-14 08:27:26 -0500

    prueba

--- login.php
+++ login.php
@@ -77,7 +77,7 @@ if ($_POST) {
     <form method="POST">
         <input type="text" name="usuario" placeholder="Usuario" required>
         <input type="password" name="clave" placeholder="Contraseña" required>
-        <button type="submit">Acceder</button>
+        <button type="submit">Accederasdfghjklñ</button>
     </form>
 </div>
 
